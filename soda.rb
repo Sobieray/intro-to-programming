8837da6ec62322eaf3a452f1544c1ca4ceca255e
Sobieray <matthew.sobieray@colorado.edu>
2012-10-17 16:30:37 -0600

    My Soda Machine Code
    intro to prgramming Week 5 HW
    code that works like a soda machine

---
+++ soda.rb
@@ -0,0 +1,50 @@
+
+coke = {"Coke" => 10, "Sprite" => 9, "Gingerale" => 5, "Fanta" => 15, "Diet" => 7}
+
+def not_empty(machine)
+	machine.each do |soda, quanity|
+		return true if quanity > 0 
+	end 
+	return false
+end
+	
+while not_empty(coke)
+
+
+			while true
+			puts "What type of soda do you want?"
+			type = gets.chomp.capitalize
+				if coke.has_key?(type)
+					puts "ok we have that"
+					break
+				else 
+					puts "we don't have that"
+				end
+			end
+
+			while true
+			puts "How many #{type}s would you like?"
+			amount = gets.chomp.to_i
+				if coke[type] >= amount
+					puts "You want #{amount} #{type}s"
+					break
+				elsif coke[type] < amount
+					puts "We don't have that many"
+				end
+			end
+
+			while 
+				if coke[type] >= amount
+					puts "There are #{coke[type]} #{type}s in the machine"
+					puts "You ordered #{amount} #{type}s"
+					coke[type] -= amount
+					puts "There are #{coke[type]} left in the machine"
+				end
+			end
+
+	puts "#{coke}"
+
+end
+
+
+
